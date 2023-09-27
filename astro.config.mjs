cfc81ac1287ddee12797c6da0874854a7e0683b0
mimi1634 <chihyu.mimichen@gmail.com>
2023-09-28 01:30:09 +0200

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,8 @@ import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://chihyu-mimichen.github.io', 
+  site: 'https://chihyu-mimichen.github.io',
+  out: 'dist',
   integrations: [mdx(), sitemap(), tailwind(), image(
     {
       serviceEntryPoint: '@astrojs/image/sharp',
